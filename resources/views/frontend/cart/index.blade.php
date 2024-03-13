60f4285372ef7560f719b84bc474068d5a921547
heshamstrak <heshamfox955@gmail.com>
2024-03-13 12:45:12 +0200

    update in color in frontend

--- resources/views/frontend/cart/index.blade.php
+++ resources/views/frontend/cart/index.blade.php
@@ -243,7 +243,7 @@
         --wp--preset--color--primary: #023373;
         --wp--preset--color--secondary: #eb4694;
         --wp--preset--color--tertiary: #ebd4ae;
-        --wp--preset--color--body-bg: #66f3ed;
+        --wp--preset--color--body-bg: #fff;
         --wp--preset--color--body-text: #4b4b4b;
         --wp--preset--color--alternate: #023373;
         --wp--preset--color--transparent: rgba(0, 0, 0, 0);
@@ -627,7 +627,7 @@
         --mfxSecondaryColorRgb: 235, 70, 148;
         --mfxTertiaryColor: #ebd4ae;
         --mfxTertiaryColorRgb: 235, 212, 174;
-        --mfxBodyBGColor: #66f3ed;
+        --mfxBodyBGColor: #fff;
         --mfxBodyBGColorRgb: 245, 234, 216;
         --mfxBodyTxtColor: #4b4b4b;
         --mfxBodyTxtColorRgb: 75, 75, 75;
@@ -5489,7 +5489,7 @@
     </noscript>
     <style type="text/css" id="custom-background-css">
     body.custom-background {
-        background-color: #66f3ed;
+        background-color: #fff;
     }
     </style>
     <link rel="icon" href="https://milmaa.wpengine.com/wp-content/uploads/2021/12/favicon.png" sizes="32x32"/>
